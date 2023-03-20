ad0f7d4564f0419f616a1ab8f3d7d183c5ad4cf5
Spicer Matthews <spicer@cloudmanic.com>
2023-03-20 17:25:23 +0000

    just messing around with the ipad

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,11 +1,10 @@
 import Link from "next/link";
 
 export default function Settings() {
-    return (
-      <>
-        <h1>Settings</h1>
-        <Link href="/">Home</Link>
-      </>
-    )
-  }
-  
+  return (
+    <>
+      <h1>Settings - From Ipad</h1>
+      <Link href="/">Home</Link>
+    </>
+  );
+}
